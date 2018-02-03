a0ef769948c8ac1b72c242f85c30f1a88df25d19
NN <nn1436401@gmail.com>
2018-02-03 19:40:27 +0200

    Initial Window, Document and HTMLElement interfaces.

--- types/lib.ie/index.d.ts
+++ types/lib.ie/index.d.ts
@@ -3,3 +3,33 @@
 // Definitions by: NN <https://github.com/NN--->
 // Definitions: https://github.com/NN---/lib.ie.d.ts
 
+interface EventTarget {
+	attachEvent(type: string, listener: EventListener): boolean;
+	detachEvent(type: string, listener: EventListener): void;
+}
+
+interface IEDocumentEventMap {
+	"onreadystatechange": Event
+}
+
+interface IEWindowEventMap {
+	"onload": Event
+}
+
+interface Document {
+	attachEvent<K extends keyof IEDocumentEventMap>(type: K, listener: (this: Window, ev: IEDocumentEventMap[K]) => any): void;
+	attachEvent(type: string, listener: EventListener): void;
+	detachEvent<K extends keyof IEDocumentEventMap>(type: K, listener: (this: Window, ev: IEDocumentEventMap[K]) => any): void;
+	detachEvent(type: string, listener: EventListener): void;
+}
+
+interface Window {
+	attachEvent<K extends keyof IEWindowEventMap>(type: K, listener: (this: Window, ev: IEWindowEventMap[K]) => any): void;
+	attachEvent(type: string, listener: EventListener): void;
+	detachEvent<K extends keyof IEWindowEventMap>(type: K, listener: (this: Window, ev: IEWindowEventMap[K]) => any): void;
+	detachEvent(type: string, listener: EventListener): void;
+}
+
+interface HTMLElement {
+	doScroll(component: string): boolean;
+}
