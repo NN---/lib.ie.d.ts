1ef347e05f5ab4dfcb8b02a487a9e651d4209d2a
NN <nn1436401@gmail.com>
2018-02-03 19:04:01 +0200

    Initial files.

---
+++ types/lib.ie/index.d.ts
@@ -0,0 +1,5 @@
+// Type definitions for IE 0.0.1
+// Project: https://github.com/NN---/lib.ie.d.ts
+// Definitions by: NN <https://github.com/NN--->
+// Definitions: https://github.com/NN---/lib.ie.d.ts
+
